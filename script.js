f104c6ce140fec47a71eef9ccfaf346bed6364e1
nanibot1 <114458365+nanibot1@users.noreply.github.com>
2023-11-29 11:57:04 -0800

    change

--- script.js
+++ script.js
@@ -3,10 +3,37 @@ const lname = document.getElementById("lname");
 const email = document.getElementById("email");
 const comment = document.getElementById("comment");
 
-fname.addEventListener("input", (event) => {
-    if(!email.checkValidity()){
-        email.setCustomValidity("First name required!");   
+const form_errors = new Array();
+
+fname.addEventListener("submit", (event) => {
+    if(!fname.checkValidity()){
+        fname.setCustomValidity("First name required!");   
+        form_errors.push("fname_error");
         event.preventDefault(); 
+        event.reportValidity();
+    }
+    else{
+        fname.setCustomValidity("");
+    }
+});
+
+lname.addEventListener("submit", (event) => {
+    if(!lname.checkValidity()){
+        lname.setCustomValidity("Last name required!");   
+        form_errors.push("lname_error");
+        event.preventDefault(); 
+        event.reportValidity();
+    }
+    else{
+        lname.setCustomValidity("");
+    }
+});
+
+email.addEventListener("submit", (event) => {
+    if(!email.checkValidity()){
+        email.setCustomValidity("Email address required!");    
+        form_errors.push("email_error");
+        event.preventDefault();
         event.reportValidity();
     }
     else{
@@ -14,32 +41,23 @@ fname.addEventListener("input", (event) => {
     }
 });
 
-lname.addEventListener("input", (event) => {
-    if(!email.checkValidity()){
-        email.setCustomValidity("Last name required!");   
-        event.preventDefault(); 
+comment.addEventListener("submit", (event) => {
+    if(!comment.checkValidity()){
+        comment.setCustomValidity("Comment required!");    
+        form_errors.push("comment_error");
+        event.preventDefault();
+        event.reportValidity();
     }
     else{
-        email.setCustomValidity("");
+        comment.setCustomValidity("");
     }
 });
 
-email.addEventListener("input", (event) => {
-    if(!email.checkValidity()){
-        email.setCustomValidity("Email address required!");    
-        event.preventDefault();
-    }
-    else{
-        email.setCustomValidity("");
-    }
-});
-
-comment.addEventListener("input", (event) => {
-    if(!email.checkValidity()){
-        email.setCustomValidity("Comment required!");    
-        event.preventDefault();
-    }
-    else{
-        email.setCustomValidity("");
-    }
+const resp = await fetch('https://httpbin.org/post', {
+  method: 'POST',
+  mode: 'cors',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify(form_errors)
 });
